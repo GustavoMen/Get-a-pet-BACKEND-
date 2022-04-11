65a14dcd463d51cbef3ecb6b976666b8a10b23ef
Gustavo Mendonça <contato.gustavomendonca@gmail.com>
2022-04-11 20:26:06 -0300

    Estruturando o projeto

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+// Config Json Response
+app.use(express.json())
+
+// Solve CORS
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+
+// Public folder for images
+app.use(express.static('public'))
+
+// Routes
+
+app.listen(5000)
---
+++ models/Pet.js
@@ -0,0 +1,39 @@
+const { required } = require('nodemon/lib/config')
+const mongoose = require('../db/conn')
+const { Schema } = mongoose
+
+const Pet = mongoose.model(
+    'Pet',
+    new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,
+            required: true
+        },
+        weigth: {
+            type: Number,
+            required: true
+        },
+        color: {
+            type: String,
+            required: true
+        },
+        images: {
+            type: Array,
+            required: true
+        },
+        available: {
+            type: Boolean
+        },
+    user: Object,
+    adopter: Object    
+    },
+    { timestamps: true},    
+  )
+)
+
+module.exports = Pet
