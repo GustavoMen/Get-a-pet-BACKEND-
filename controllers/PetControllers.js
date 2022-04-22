90f528853caed746f78bfa8464bd07cdb8c2ca21
Gustavo Mendonça <contato.gustavomendonca@gmail.com>
2022-04-22 19:22:34 -0300

    correção de mensagem

--- controllers/PetControllers.js
+++ controllers/PetControllers.js
@@ -226,10 +226,7 @@ module.exports = class PetController {
             updatedData.color = color
         }
 
-        if(images.length === 0) {
-            res.status(422).json({message: 'A imagem é obrigatoria!'})
-            return
-        }else{
+        if(images.length > 0) {
             updatedData.images = []
             images.map((image) => {
                 updatedData.images.push(image.filename)
